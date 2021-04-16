ae8b41caad23cee1d9f3d7f816fed866d373954b
Kate Volkova <let.kate.test@gmail.com>
2021-04-16 14:26:55 +0200

    Complete classes for GHappy and GHappyTest

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,15 +4,17 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
+        int g = 0;
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
+                g++;
+                if (i > 0 && str.charAt(i-1) == 'g') { continue; }
                 if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
                 return false;
             }
         }
 
-        return true;
+        return g == 0 ? false : true;
 
     }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,25 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.ghappy.GHappy;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "{0}: ({1}) = {2}")
+    @CsvSource({
+            // A string without G but with another double character.
+            "'withoutG','xxyyxx',false",
+            // One G in different positions in the string.
+            "'1g','gxxyyxx',false", "'1g','xxyyxxg',false", "'1g','xxgyyxx',false",
+            // Two G in different positions, together and separate.
+            "'2gg','xxyggyxx',true", "'2gg','ggxxyyxx',true", "'2gg','xxyyxxgg',true", "'2gg','xxygagyxx',false",
+            // Three G in different positions, together and separate.
+            "'3ggg','xxygggyxx',true", "'3ggg','xxygyxxgg',false", "'3ggg','xggxygyxx',false"
+    })
+    public void testAlgorithm(String partition, String str, Boolean expectedResult) {
+        Boolean result = new GHappy().gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
 }
