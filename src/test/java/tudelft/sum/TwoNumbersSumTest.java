2fce5ee31a39c6a432050364b84e69e864747ac6
Kate Volkova <let.kate.test@gmail.com>
2021-05-05 11:18:44 +0200

    New tests for TwoNumbersSum and fixed error in the main program

--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,62 @@
 package tudelft.sum;
 
-public class TwoNumbersSumTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+public class TwoNumbersSumTest {
+    @Test
+    public void threeAndTwoNumbers () {
+        TwoNumbersSum sumResult = new TwoNumbersSum();
+        ArrayList <Integer> first = new ArrayList <Integer>();
+        first.add(1);
+        first.add(2);
+        first.add(3);
+        ArrayList <Integer> second = new ArrayList <Integer>();
+        second.add(2);
+        second.add(1);
+        ArrayList<Integer> result = sumResult.addTwoNumbers (first,second);
+        ArrayList <Integer> actRes = new ArrayList <Integer>();
+        actRes.add(1);
+        actRes.add(4);
+        actRes.add(4);
+        Assertions.assertEquals(actRes, result);
+    }
+
+    @Test
+    public void zeroLoop () {
+        TwoNumbersSum sumResult = new TwoNumbersSum();
+        ArrayList <Integer> first = new ArrayList <Integer>();
+        ArrayList <Integer> second = new ArrayList <Integer>();
+        ArrayList<Integer> result = sumResult.addTwoNumbers (first,second);
+        ArrayList <Integer> actRes = new ArrayList <Integer>();
+        Assertions.assertEquals(actRes, result);
+    }
+    @Test
+    public void oneLoop () {
+        TwoNumbersSum sumResult = new TwoNumbersSum();
+        ArrayList <Integer> first = new ArrayList <Integer>();
+        first.add(2);
+        ArrayList <Integer> second = new ArrayList <Integer>();
+        second.add(7);
+        ArrayList<Integer> result = sumResult.addTwoNumbers (first,second);
+        ArrayList <Integer> actRes = new ArrayList <Integer>();
+        actRes.add(9);
+        Assertions.assertEquals(actRes, result);
+    }
+
+    @Test
+    public void totalEqualsTen () {
+        TwoNumbersSum sumResult = new TwoNumbersSum();
+        ArrayList <Integer> first = new ArrayList <Integer>();
+        first.add(6);
+        ArrayList <Integer> second = new ArrayList <Integer>();
+        second.add(4);
+        ArrayList<Integer> result = sumResult.addTwoNumbers (first,second);
+        ArrayList <Integer> actRes = new ArrayList <Integer>();
+        actRes.add(1);
+        actRes.add(0);
+        Assertions.assertEquals(actRes, result);
+    }
 }
