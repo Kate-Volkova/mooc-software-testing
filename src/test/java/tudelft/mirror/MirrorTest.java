fa7a9d0fb8ccd6d7cb1b89305cbddd9531286d7d
Kate Volkova <let.kate.test@gmail.com>
2021-05-06 12:52:48 +0200

    MirrorTest added

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,49 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+    @Test
+    void oneLoop() {
+        Mirror string = new Mirror();
+        String mirror = string.mirrorEnds("t");
+        Assertions.assertEquals("t", mirror);
+    }
+
+    @Test
+    void noMirrorLetters() {
+        Mirror string = new Mirror();
+        String mirror = string.mirrorEnds("tffa");
+        Assertions.assertEquals("", mirror);
+    }
+
+    @Test
+    void emptyStringZeroLoop() {
+        Mirror string = new Mirror();
+        String mirror = string.mirrorEnds("");
+        Assertions.assertEquals("", mirror);
+    }
+
+    @Test
+    void severalLoops() {
+        Mirror string = new Mirror();
+        String mirror = string.mirrorEnds("daghad");
+        Assertions.assertEquals("da", mirror);
+    }
+
+    @Test
+    void totalMirror() {
+        Mirror string = new Mirror();
+        String mirror = string.mirrorEnds("dagad");
+        Assertions.assertEquals("dagad", mirror);
+    }
+
+    @Test
+    void lettersWithSpaces() {
+        Mirror string = new Mirror();
+        String mirror = string.mirrorEnds("d dhd");
+        Assertions.assertEquals("d", mirror);
+    }
+
 }
